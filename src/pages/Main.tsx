ff9d0cb10c1937ceb990ab282761112aa7d1aab7
Iranigami <iranigami@mail.ru>
2025-06-11 10:54:06 +0400

    add images slides?

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,7 @@
 
 import { useNavigate } from "react-router-dom";
 import pickIcon from "../assets/icons/pick.svg"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type Props = {
     onWaiting: boolean;
@@ -10,12 +10,31 @@ type Props = {
 
 
 export default function Main({onWaiting, onClicked}: Props){
+    const images = [
+        "https://dovosp.ru/wp-content/uploads/2022/03/zastavka-1.jpg",
+        "https://img.geliophoto.com/mzhdr/00_mzhdr.jpg",
+        "https://indieview.ru/wp-content/uploads/2023/08/01-2-gorod-mezhdurechensk-1024x683.jpg",
+        "https://avatars.mds.yandex.net/i?id=367392952aa527ea02e7b250ea7d2895_l-5252264-images-thumbs&n=13"
+    ]
     const[keepOpen, setKeepOpen] = useState(true);
     const navigate = useNavigate();
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const changeSlide = () => {
+        setTimeout(() => {
+            setCurrentSlide(prev => prev<images.length-1 ? prev + 1 : 0);
+            changeSlide();
+        }, 15000)
+        
+    }
+    useEffect(() => {
+        changeSlide();
+    }, [])
     return(
         <div hidden={!keepOpen} className="fixed z-2 top-0 w-[2160px] h-[3840px]">
             <div className={`absolute w-full h-full bg-white z-[-1] duration-700 transition ${!onWaiting && "translate-y-[-4200px] opacity-0 scale-[150%]"}`}>
-                <img src="https://img.geliophoto.com/mzhdr/00_mzhdr.jpg" alt="img" className="object-cover w-full h-full" />
+                {images.map((image, index: number) => (
+                    <img key={index} src={image} alt="img" className={`object-cover w-full h-full absolute duration-1000 transition ${currentSlide === index ? "opacity-100" : "opacity-0"}`} />
+                ))}
             </div>
             <button 
                 onClick={()=>{
