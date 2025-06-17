052205e22a58f91807ffe379fa70c236882f09d6
Iranigami <iranigami@mail.ru>
2025-06-17 13:40:22 +0400

    add video support

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -8,18 +8,34 @@ type Props = {
 };
 
 export default function Main({ onWaiting, onClicked }: Props) {
-  const images = [
-    "https://dovosp.ru/wp-content/uploads/2022/03/zastavka-1.jpg",
-    "https://img.geliophoto.com/mzhdr/00_mzhdr.jpg",
-    "https://indieview.ru/wp-content/uploads/2023/08/01-2-gorod-mezhdurechensk-1024x683.jpg",
-    "https://avatars.mds.yandex.net/i?id=367392952aa527ea02e7b250ea7d2895_l-5252264-images-thumbs&n=13",
+  const media = [
+    {
+      source: "https://dovosp.ru/wp-content/uploads/2022/03/zastavka-1.jpg",
+      type: "image",
+    },
+    {
+      source: "https://img.geliophoto.com/mzhdr/00_mzhdr.jpg",
+      type: "image",
+    },
+    {
+      source: "https://indieview.ru/wp-content/uploads/2023/08/01-2-gorod-mezhdurechensk-1024x683.jpg",
+      type: "image",
+    },
+    {
+      source: "https://avatars.mds.yandex.net/i?id=367392952aa527ea02e7b250ea7d2895_l-5252264-images-thumbs&n=13",
+      type: "image",
+    },
+    {
+      source: "https://cdn-front.freepik.com/videos/media/cover-v2-3.webm",
+      type: "video",
+    },
   ];
   const [keepOpen, setKeepOpen] = useState(true);
   const navigate = useNavigate();
   const [currentSlide, setCurrentSlide] = useState(0);
   const changeSlide = () => {
     setTimeout(() => {
-      setCurrentSlide((prev) => (prev < images.length - 1 ? prev + 1 : 0));
+      setCurrentSlide((prev) => (prev < media.length - 1 ? prev + 1 : 0));
       changeSlide();
     }, 15000);
   };
@@ -33,13 +49,26 @@ export default function Main({ onWaiting, onClicked }: Props) {
           <div
             className={`absolute w-full h-full bg-white z-[-1] duration-700 transition ${!onWaiting && "translate-y-[-4200px] opacity-0 scale-[150%]"}`}
           >
-            {images.map((image, index: number) => (
-              <img
-                key={index}
-                src={image}
+            {media.map((file, index: number) => (
+              <div
+              key={index}>
+              {
+                file.type==="image" && <img
+                src={file.source}
                 alt="img"
                 className={`object-cover w-full h-full absolute duration-1000 transition ${currentSlide === index ? "opacity-100" : "opacity-0"}`}
               />
+              }
+              {
+                file.type==="video" && <video
+                controls loop
+                muted
+                autoPlay
+                src={file.source}
+                className={`object-cover w-full h-full absolute duration-1000 transition ${currentSlide === index ? "opacity-100" : "opacity-0"}`}
+              />
+              }
+              </div>
             ))}
             <div className="absolute w-[2160px] h-[807px] opacity-70 bg-linear-to-b from-[#04341C] to-[#04341C00]" />
             <div className="absolute bottom-0 w-[2160px] h-[807px] opacity-70 bg-linear-to-t from-[#04341C] to-[#04341C00]" />
